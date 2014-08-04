d5b44f2d9d1f14697359c6a72e7c00fa72f40d7b
Jeff Maher <jeff@codeforamerica.org>
2014-08-04 16:29:43 -0700

    Added skeleton code for running the lottery

--- ripl-console/Program.cs
+++ ripl-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Ripl.Reader;
 using Ripl.Csv;
+using Ripl.Lottery;
 using Ripl.Model;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ripl.Calc;
+using System.IO;
+using CsvHelper;
 
 namespace Ripl
 {
@@ -20,6 +23,37 @@ namespace Ripl
         //*********************************************
         // Throw away temp methods
         //*********************************************
+
+        public static void RunLottery()
+        {
+            // Read in school configurations
+            SchoolReader schoolReader = new SchoolCsvReader("c:\\users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\schools.csv");
+            List<School> schools = schoolReader.ReadSchools();
+
+            // Read in applicants and split across schools applied for
+            IncomeReader incomeReader = new IncomeCsvReader("C:\\Users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\income.csv");
+            IncomeCalculator incomeCalc = new IncomeCalculator(incomeReader);
+
+            ApplicantReader applicantReader = new ApplicantCsvReader("C:\\temp\\data.csv", schools, incomeCalc);
+            applicantReader.ReadApplicants();
+
+            // Perform the lottery for each school
+            var settings = ConfigurationManager.AppSettings;
+            int numStudentsPerClassroom = int.Parse(settings["numStudentsPerClassroom"]);
+            double percentMale = double.Parse(settings["percentMale"]);
+            SchoolLottery schoolLottery = new SchoolLottery(numStudentsPerClassroom, percentMale);
+
+            //TODO Move this to the Ripl.Csv package
+            foreach(School school in schools)
+            {
+                string filePath = "C:\\temp\\" + school.Name + ".csv";
+                using (StreamWriter textWriter = new StreamWriter(filePath))
+                {
+                    CsvWriter csvWriter = new CsvWriter(textWriter);
+                    csvWriter.WriteRecords(school.Applicants);
+                }
+            }
+        }
 
         public static void ReadIncome()
         {
@@ -72,6 +106,7 @@ namespace Ripl
 
         public static void ReadAppSettings()
         {
+            // TODO switch to a JSON or XML configuration
             var settings = ConfigurationManager.AppSettings;
             int numStudentsPerClassroom = int.Parse(settings["numStudentsPerClassroom"]);
             Console.WriteLine(numStudentsPerClassroom);
--- ripl-console/Ripl.Lottery/SchoolLottery.cs
+++ ripl-console/Ripl.Lottery/SchoolLottery.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using Ripl.Model;
+using System;
 using System.Collections.Generic;
 
 namespace Ripl.Lottery
 {
     public class SchoolLottery
     {
+        int studentsPerClassroom;
+        double percentMale;
+
+        public SchoolLottery(int studentsPerClassroom, double percentMale)
+        {
+            this.studentsPerClassroom = studentsPerClassroom;
+            this.percentMale = percentMale;
+        }
+
+        public School Run(School school)
+        {
+            return null;
+        }
     }
 }
