8364a51c9174d5f30b3b71193ef2c76dd1aa0da8
Jeff Maher <jeff@codeforamerica.org>
2014-08-05 21:51:45 -0700

    Added console arguments to Main and cleaned up unused methods.

--- ripl-console/Program.cs
+++ ripl-console/Program.cs
@@ -18,22 +18,25 @@ namespace Ripl
     {
         public static void Main(string[] args)
         {
-            RunLottery();
-            //RunLotteryWithoutCrossSchoolReconciliation();
+            string schoolConfigPath = args[0];
+            string incomeConfigPath = args[1];
+            string applicantCsvPath = args[2];
+
+            RunLottery(schoolConfigPath, incomeConfigPath, applicantCsvPath);
         }
 
         //TODO cleanup -- works fine, just sloppy
-        public static void RunLottery()
+        public static void RunLottery(string schoolConfigPath, string incomeConfigPath, string applicantCsvPath)
         {
             // Read in school configurations
-            SchoolReader schoolReader = new SchoolCsvReader("c:\\users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\schools.csv");
+            SchoolReader schoolReader = new SchoolCsvReader(schoolConfigPath);
             List<School> schools = schoolReader.ReadSchools();
 
             // Read in applicants and split across schools applied for
-            IncomeReader incomeReader = new IncomeCsvReader("C:\\Users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\income.csv");
+            IncomeReader incomeReader = new IncomeCsvReader(incomeConfigPath);
             IncomeCalculator incomeCalc = new IncomeCalculator(incomeReader);
 
-            ApplicantReader applicantReader = new ApplicantCsvReader("C:\\temp\\data.csv", schools, incomeCalc);
+            ApplicantReader applicantReader = new ApplicantCsvReader(applicantCsvPath, schools, incomeCalc);
             applicantReader.ReadApplicants();
 
             // Perform the lottery for each school
@@ -72,111 +75,5 @@ namespace Ripl
                     csvWriter.WriteRecords(schoolRun.WaitlistedApplicants);
                 }
             }
-
         }
-
-
-        //*********************************************
-        // Throw away temp methods, used during dev
-        //*********************************************
-        public static void RunLotteryWithoutCrossSchoolReconciliation()
-        {
-            // Read in school configurations
-            SchoolReader schoolReader = new SchoolCsvReader("c:\\users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\schools.csv");
-            List<School> schools = schoolReader.ReadSchools();
-
-            // Read in applicants and split across schools applied for
-            IncomeReader incomeReader = new IncomeCsvReader("C:\\Users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\income.csv");
-            IncomeCalculator incomeCalc = new IncomeCalculator(incomeReader);
-
-            ApplicantReader applicantReader = new ApplicantCsvReader("C:\\temp\\data.csv", schools, incomeCalc);
-            applicantReader.ReadApplicants();
-
-            // Perform the lottery for each school
-            var settings = ConfigurationManager.AppSettings;
-            int numStudentsPerClassroom = int.Parse(settings["numStudentsPerClassroom"]);
-            double percentMale = double.Parse(settings["percentMale"]);
-            DateTime age4ByDate = Convert.ToDateTime(settings["age4ByDate"]);
-            SchoolLottery schoolLottery = new SchoolLottery(numStudentsPerClassroom, percentMale, age4ByDate);
-
-            //TODO Move this to the Ripl.Csv package
-            foreach (School school in schools)
-            {
-                var schoolRun = schoolLottery.Run(school);
-
-                // Selected
-                string selectedFilePath = "C:\\temp\\selected\\" + school.Name + ".csv"; //TODO use arguments
-                using (StreamWriter textWriter = new StreamWriter(selectedFilePath))
-                {
-                    CsvWriter csvWriter = new CsvWriter(textWriter);
-                    csvWriter.WriteRecords(schoolRun.SelectedApplicants);
-                }
-
-                // Wait List
-                string waitListedFilePath = "C:\\temp\\wait\\" + school.Name + ".csv"; //TODO use arguments
-                using (StreamWriter textWriter = new StreamWriter(waitListedFilePath))
-                {
-                    CsvWriter csvWriter = new CsvWriter(textWriter);
-                    csvWriter.WriteRecords(schoolRun.WaitlistedApplicants);
-                }
-            }
-        }
-
-        public static void ReadIncome()
-        {
-            IncomeReader incomeReader = new IncomeCsvReader("C:\\Users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\income.csv");
-            var incomeLevels = incomeReader.ReadIncome();
-
-            foreach(int numHouseholdMembers in incomeLevels.Keys)
-            {
-                Console.WriteLine("{0} --> {1}", numHouseholdMembers, incomeLevels[numHouseholdMembers]);
-            }
-        }
-
-        public static void ReadApplicants()
-        {
-            SchoolReader schoolReader = new SchoolCsvReader("c:\\users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\schools.csv");
-            List<School> schools = schoolReader.ReadSchools();
-
-            IncomeReader incomeReader = new IncomeCsvReader("C:\\Users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\income.csv");
-            IncomeCalculator incomeCalc = new IncomeCalculator(incomeReader);
-
-            ApplicantReader applicantReader = new ApplicantCsvReader("C:\\temp\\data.csv", schools, incomeCalc);
-            applicantReader.ReadApplicants();
-
-            foreach(School school in schools)
-            {
-                Console.WriteLine("***************");
-                Console.WriteLine("School: {0}", school.Name);
-                
-                foreach(Applicant applicant in school.Applicants)
-                {
-                    Console.WriteLine(" - {0} {1} : {2}", applicant.StudentFirstName, applicant.StudentLastName, applicant.IsBelowPovertyLevel);
-                }
-            }
-        }
-
-        public static void ReadSchoolSettings()
-        {
-            SchoolReader schoolReader = new SchoolCsvReader("c:\\users\\jeff\\code\\ripl-console\\ripl-console\\samples\\config\\schools.csv");
-            List<School> schools = schoolReader.ReadSchools();
-
-            foreach(School school in schools)
-            {
-                Console.WriteLine("**************************");
-                Console.WriteLine("Name: {0}", school.Name);
-                Console.WriteLine("Classrooms: {0}", school.NumClassrooms);
-                Console.WriteLine("District: {0}", school.District);
-                Console.WriteLine("Percent Below Poverty Line: {0}", school.PercentBelowPovertyLine);
-            }
-        }
-
-        public static void ReadAppSettings()
-        {
-            // TODO switch to a JSON or XML configuration
-            var settings = ConfigurationManager.AppSettings;
-            int numStudentsPerClassroom = int.Parse(settings["numStudentsPerClassroom"]);
-            Console.WriteLine(numStudentsPerClassroom);
-        }
-    }
 }
